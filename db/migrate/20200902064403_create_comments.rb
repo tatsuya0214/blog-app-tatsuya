92d8903de83d60ef8905b966e299491e7c4b3aba
石川竜也 <>
2020-09-02 22:10:14 +0900

    createアクション

--- db/migrate/20200902064403_create_comments.rb
+++ db/migrate/20200902064403_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.references :article, null: false
-      t.text :comment, null: false
+      t.text :content, null: false
       t.timestamps
     end
   end
